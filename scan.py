0ceeb5ba3fa49005e0c1633aff7e3f727ac9e465
VISWESWARAN1998 <visweswaran.nagasivam98@gmail.com>
2017-11-20 20:44:22 +0530

    Quick run

--- scan.py
+++ scan.py
@@ -1,6 +1,5 @@
 # SWAMI KARUPPASWAMI THUNNAI
 
-import socket
 import pymysql
 import threading
 from information_gathering_phase1 import web_server_informtaion
@@ -17,11 +16,14 @@ def check_for_vulnerabilities(connection, project_id, url):
                                                                         database_semaphore=database_semaphore, url=url)
     information_gathering.gather_information()
 
-def scan(project_id, url):
-    connection = pymysql.connect(host="localhost", user="root", password="YOUR PASSWORD HERE",
+
+def main():
+    print("Siva Vulnerability Scanner v1.0")
+    password = input("Enter the password: ")
+    connection = pymysql.connect(host="localhost", user="root", password=password,
                                  db="siva")
-    project_id = project_id
-    url = url
+    project_id = int(input("Project ID: "))
+    url = input("URL: ")
     scheme = URL().get_scheme(url)
     host_name = URL().get_host_name(url)
     project_url = scheme + "://" + host_name
@@ -29,23 +31,6 @@ def scan(project_id, url):
         check_for_vulnerabilities(connection, project_id, url)
     else:
         print("Cannot be scanned!")
-
-def main():
-    print("Siva Vulnerability Scanner v0.1")
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = int(input("Enter the port: "))
-    server.bind(("127.0.0.1", port))
-    server.listen(5)
-    while True:
-        client, address = server.accept()
-        message = client.recv(4000)
-        parameters = message.decode("utf-8")
-        parameters = parameters.split(" ")
-        project_id = parameters[0]
-        url = parameters[1]
-        scan(project_id, url)
-        server.close()
-        break
 
 
 
